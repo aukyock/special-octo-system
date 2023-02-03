8a1f82e85a5e6c2ef8fbeb76c53da50591260221
aukyock <okochi.201045@gmail.com>
2023-02-03 19:55:57 +0900

    消してしまったブログ投稿詳細画面追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
-
 {
     public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
     public function create()
     {
         return view('posts/create');
     }
-    public function store(PostRequest $request, Post $post)
+    
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
